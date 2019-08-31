c3c16a77066f32a678d875cd8f1fde96b724a2c1
Chengxu Zhuang <chengxuz@node07-ccncluster.stanford.edu>
2019-08-31 11:16:25 -0700

    Minor

--- train_res18.sh
+++ train_res18.sh
@@ -1,4 +1,4 @@
-python train_CMC.py --data_folder /data5/chengxuz/imagenet_raw \
+python train_CMC.py --data_folder /data5/chengxuz/Dataset/imagenet_raw \
     --model_path /mnt/fs4/chengxuz/cmc_models/res18_half/models \
     --tb_path /mnt/fs4/chengxuz/cmc_models/res18_half/recs \
-    --model resnet18 --batch_size 128 --crop_low 0.08 --is_half
+    --model resnet18 --batch_size 128 --crop_low 0.08 --is_half "$@"
